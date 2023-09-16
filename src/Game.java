c81a20a512092f764a4770caef665568b593c85e
Aleksey <aleksejbaskirov31@gmail.com>
2023-09-17 00:56:00 +0300

    Add Arkade Game

--- src/Game.java
+++ src/Game.java
@@ -1,142 +1,142 @@
-//import java.awt.*;
-//import java.awt.event.*;
-//
-//public class Game extends Framework {
-//    protected void doInitialization (int width, int height) {
-//        initSubmarine(width, height);
-//        initBoat(width, height);
-//        initBomb(width, height);
+import java.awt.*;
+import java.awt.event.*;
+
+public class Game extends Framework {
+    protected void doInitialization (int width, int height) {
+        initSubmarine(width, height);
+        initBoat(width, height);
+        initBomb(width, height);
+    }
+    synchronized public void drawFrame(Graphics g, int width, int height){
+        g.setColor(Color.green);
+        g.fillRect(0, 0, width, height);
+        doBoatFrame(g, width, height);
+        doSubmarineFrame(g, width, height);
+        doBombFrame(g, width, height);
+    }
+    synchronized public void keyPressed(KeyEvent evt) {
+        int code = evt.getKeyCode();
+        if (code == KeyEvent.VK_LEFT) {
+            boatCenterX = -15;
+        }
+        else if (code == KeyEvent.VK_RIGHT) {
+            boatCenterX = +15;
+        }
+        else if (code == KeyEvent.VK_DOWN) {
+            if (!bombIsFalling)
+                bombIsFalling = true;
+        }
+    }
+    int bombCenterX;
+    int bombCenterY;
+    boolean bombIsFalling;
+    void initBomb(int width, int height) {
+        bombIsFalling = false;
+        bombCenterY = 100;
+    }
+    void doBombFrame(Graphics g, int width, int height){
+        if (bombIsFalling) {
+            if (bombCenterY > height) {
+                initBomb(width, height);
+                bombCenterX = boatCenterX;
+            }
+            else if (Math.abs(bombCenterX - subCenterX) <= 36 &&
+            Math.abs(bombCenterY - subCenterY) <= 21) {
+                explosionFrameNumber = 1;
+                initBomb(width, height);
+                bombCenterX = boatCenterX;
+            }
+            else {
+                bombCenterY += 10;
+                g.setColor(Color.red);
+                g.fillOval(bombCenterX - 8, bombCenterY - 8, 16, 16);
+            }
+        }
+        else {
+            bombCenterX = boatCenterX;
+            g.setColor(Color.red);
+            g.fillOval(bombCenterX - 8, bombCenterY - 8, 16,16);
+        }
+    }
+    int boatCenterX;
+    int boatCenterY;
+    void initBoat(int width, int height){
+        boatCenterX = width/2;
+        boatCenterY = 80;
+    }
+    private void doBoatFrame(Graphics g, int width, int height) {
+        if (boatCenterX < 0)
+            boatCenterX = 0;
+        else if (boatCenterX > width)
+            boatCenterX = width;
+        g.setColor(Color.blue);
+        g.fillRoundRect(boatCenterX - 40, boatCenterY - 20, 80, 40, 20, 20);
+    }
+    int subCenterX;
+    int subCenterY;
+    boolean subIsMovingLeft;
+    int explosionFrameNumber;
+
+    void initSubmarine(int width, int height) {
+        subCenterX = (int)(width*Math.random());
+        subCenterY = height -40;
+        explosionFrameNumber = 0;
+        if (Math.random() < 0.5)
+            subIsMovingLeft = true;
+        else
+            subIsMovingLeft = false;
+    }
+    private void doSubmarineFrame(Graphics g, int width, int height) {
+        if (explosionFrameNumber > 0) {
+            if (explosionFrameNumber == 14) {
+                initSubmarine(width, height);
+            }
+            else if (explosionFrameNumber > 10) {
+                explosionFrameNumber++;
+            }
+            else {
+                g.setColor(Color.black);
+                g.fillOval(subCenterX - 30, subCenterY -15, 60, 30);
+                g.setColor(Color.yellow);
+                g.fillOval(subCenterX - 4*explosionFrameNumber,
+                        subCenterY - 2*explosionFrameNumber,
+                        8*explosionFrameNumber,
+                        4*explosionFrameNumber);
+                g.setColor(Color.red);
+                g.fillOval(subCenterX - 2*explosionFrameNumber,
+                        subCenterY - explosionFrameNumber/2,
+                        4*explosionFrameNumber,
+                        explosionFrameNumber);
+                explosionFrameNumber++;
+            }
+        }
+        else {
+            if (Math.random() < 0.04)
+                subIsMovingLeft = !subIsMovingLeft;
+            if (subIsMovingLeft) {
+                subCenterX -= 5;
+                if (subCenterX <= 0) {
+                    subCenterX = 0;
+                    subIsMovingLeft = false;
+                }
+            }
+            else {
+                subCenterX += 5;
+                if (subCenterX > width) {
+                    subCenterX = width;
+                    subIsMovingLeft = true;
+                }
+            }
+            g.setColor(Color.black);
+            g.fillOval(subCenterX - 30, subCenterY - 15, 60, 30);
+        }
+    }
+
+//    private void initBomb(int width, int height) {
 //    }
-//    synchronized public void drawFrame(Graphics g, int width, int height){
-//        g.setColor(Color.green);
-//        g.fillRect(0, 0, width, height);
-//        doBoatFrame(g, width, height);
-//        doSubmarineFrame(g, width, height);
-//        doBombFrame(g, width, height);
+//    private void initBoat(int width, int height) {
 //    }
-//    synchronized public void keyPressed(KeyEvent evt) {
-//        int code = evt.getKeyCode();
-//        if (code == KeyEvent.VK_LEFT) {
-//            boatCenterX = -15;
-//        }
-//        else if (code == KeyEvent.VK_RIGHT) {
-//            boatCenterX = +15;
-//        }
-//        else if (code == KeyEvent.VK_DOWN) {
-//            if (!bombIsFalling)
-//                bombIsFalling = true;
-//        }
+//    private void initSubmarine(int width, int height) {
 //    }
-//    int bombCenterX;
-//    int bombCenterY;
-//    boolean bombIsFalling;
-//    void initBomb(int width, int height) {
-//        bombIsFalling = false;
-//        bombCenterY = 100;
-//    }
-//    void doBombFrame(Graphics g, int width, int height){
-//        if (bombIsFalling) {
-//            if (bombCenterY > height) {
-//                initBomb(width, height);
-//                bombCenterX = boatCenterX;
-//            }
-//            else if (Math.abs(bombCenterX - subCenterX) <= 36 &&
-//            Math.abs(bombCenterY - subCenterY) <= 21) {
-//                explosionFrameNumber = 1;
-//                initBomb(width, height);
-//                bombCenterX = boatCenterX;
-//            }
-//            else {
-//                bombCenterY += 10;
-//                g.setColor(Color.red);
-//                g.fillOval(bombCenterX - 8, bombCenterY - 8, 16, 16);
-//            }
-//        }
-//        else {
-//            bombCenterX = boatCenterX;
-//            g.setColor(Color.red);
-//            g.fillOval(bombCenterX - 8, bombCenterY - 8, 16,16);
-//        }
-//    }
-//    int boatCenterX;
-//    int boatCenterY;
-//    void initBoat(int width, int height){
-//        boatCenterX = width/2;
-//        boatCenterY = 80;
-//    }
-//    private void doBoatFrame(Graphics g, int width, int height) {
-//        if (boatCenterX < 0)
-//            boatCenterX = 0;
-//        else if (boatCenterX > width)
-//            boatCenterX = width;
-//        g.setColor(Color.blue);
-//        g.fillRoundRect(boatCenterX - 40, boatCenterY - 20, 80, 40, 20, 20);
-//    }
-//    int subCenterX;
-//    int subCenterY;
-//    boolean subIsMovingLeft;
-//    int explosionFrameNumber;
-//
-//    void initSubmarine(int width, int height) {
-//        subCenterX = (int)(width*Math.random());
-//        subCenterY = height -40;
-//        explosionFrameNumber = 0;
-//        if (Math.random() < 0.5)
-//            subIsMovingLeft = true;
-//        else
-//            subIsMovingLeft = false;
-//    }
-//    private void doSubmarineFrame(Graphics g, int width, int height) {
-//        if (explosionFrameNumber > 0) {
-//            if (explosionFrameNumber == 14) {
-//                initSubmarine(width, height);
-//            }
-//            else if (explosionFrameNumber > 10) {
-//                explosionFrameNumber++;
-//            }
-//            else {
-//                g.setColor(Color.black);
-//                g.fillOval(subCenterX - 30, subCenterY -15, 60, 30);
-//                g.setColor(Color.yellow);
-//                g.fillOval(subCenterX - 4*explosionFrameNumber,
-//                        subCenterY - 2*explosionFrameNumber,
-//                        8*explosionFrameNumber,
-//                        4*explosionFrameNumber);
-//                g.setColor(Color.red);
-//                g.fillOval(subCenterX - 2*explosionFrameNumber,
-//                        subCenterY - explosionFrameNumber/2,
-//                        4*explosionFrameNumber,
-//                        explosionFrameNumber);
-//                explosionFrameNumber++;
-//            }
-//        }
-//        else {
-//            if (Math.random() < 0.04)
-//                subIsMovingLeft = !subIsMovingLeft;
-//            if (subIsMovingLeft) {
-//                subCenterX -= 5;
-//                if (subCenterX <= 0) {
-//                    subCenterX = 0;
-//                    subIsMovingLeft = false;
-//                }
-//            }
-//            else {
-//                subCenterX += 5;
-//                if (subCenterX > width) {
-//                    subCenterX = width;
-//                    subIsMovingLeft = true;
-//                }
-//            }
-//            g.setColor(Color.black);
-//            g.fillOval(subCenterX - 30, subCenterY - 15, 60, 30);
-//        }
-//    }
-//
-////    private void initBomb(int width, int height) {
-////    }
-////    private void initBoat(int width, int height) {
-////    }
-////    private void initSubmarine(int width, int height) {
-////    }
-//
-//}
+
+}
--- src/LinesMove.java
+++ src/LinesMove.java
@@ -1,11 +1,68 @@
 import java.applet.Applet;
+import java.awt.*;
 
-//public class LinesMove extends Applet implements Runnable {
-//    boolean used = false; //Знаяение переменной used при первой ориентации
-//     //на курсор мыши становится равным true
-//    int ROWS = 5; // Количество Рядов
-//    int COLUMNS = 7; // количество Столбцов
-//    int [][] angle; // Угол
-//    int [][] driftSpeed; // Угловая скорость
-//
-//}
+public class LinesMove extends Applet implements Runnable {
+    boolean used = false; //Значение переменной used при первой ориентации
+    // на курсор мыши становится равным true.
+    int ROWS = 5; // Количество Рядов.
+    int COLUMNS = 7; // количество Столбцов.
+    int [][] angle; // Угол.
+    int [][] driftSpeed; // Угловая скорость.
+    long lastTime = 0; // Время последней Инициализации порядка.
+    int delayToDrift =1500; // Задержка при упорядочении.
+    int width = -1; // Ширина Апплета.
+    int height = -1; // Высота Апплета.
+    int hSpace; // Горизонтальное Расстояние между Линиями.
+    int vSpace; // Вертикальное Расстояние между Линиями.
+    int space; // Ширина Каждой Линии.
+    int last_x = -1; // Координаты Точки, на которую Линии были
+    int last_y = -1; // Ориентированы в Прошлый Раз.
+    int sleepTime = 100; // Задержка между Фреймами.
+    Thread runner = null; // Поток Анимации.
+    Image OSC = null; // Невидимый Холст для Двойной Буферизации.
+    Graphics OSCGraphics;
+    public void init() {
+        setBackground(Color.black);
+        setForeground(Color.white);
+        String param;
+        param = getParameter("rows");
+        if (param != null) {
+            try{
+                ROWS = Integer.parseInt(param);
+            }
+            catch(NumberFormatException e) { }
+        }
+        param = getParameter("columns");
+        if (param != null) {
+            try{
+                COLUMNS = Integer.parseInt(param);
+            }
+            catch(NumberFormatException e) { }
+        }
+        angle = new int[ROWS][COLUMNS];
+        driftSpeed = new int[ROWS][COLUMNS];
+        for (int i = 0; i <ROWS; i++)
+            for (int j =0; j<COLUMNS; j++) {
+                angle[i][j] = (int)(360*Math.random());
+                driftSpeed[i][j] = (int)(41*Math.random())-20;
+            }
+    }
+    public void start() {
+        if (runner == null) {
+            int h = getSize().height;
+            int w = getSize().width;
+            if (h != height || w != width)
+                doResize(w,h);
+            runner = new Thread(this);
+            runner.start();
+        }
+    }
+    public void stop() {
+        // Остановка Анимации.
+        if (runner != null) {
+            runner.stop();
+            runner.null;
+        }
+    }
+
+}
